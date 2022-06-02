0dcfe419dc65a8c272c639a69b6a856e64c904a9
Ronald <rjskinnerc@unal.edu.co>
2022-06-01 21:52:13 -0500

    Improve active link styles

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav className="md:flex justify-between items-center md:mx-5 lg:w-4/5 lg:mx-auto">
-    <h1 className="text-rose-500 text-3xl text-center py-10 font-bold" :style="{
+  <nav class="md:flex justify-between items-center md:mx-5 lg:w-4/5 lg:mx-auto">
+    <h1 class="text-rose-500 text-3xl text-center py-10 font-bold" :style="{
       textShadow: '1px 1px 1px rgba(206,206,206,0.7)'
     }">Math Magicians</h1>
     <ul className="flex justify-around md:gap-x-5">
