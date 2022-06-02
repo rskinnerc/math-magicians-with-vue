3e2c2623777a86cef88258d22a1bd425e7eca6ae
Ronald <rjskinnerc@unal.edu.co>
2022-06-01 20:57:19 -0500

    Add the home page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Calculator from './components/Calculator.vue';
 import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <div className="bg-gradient-to-r from-sky-900 to-zinc-800 min-h-screen">
     <Navbar />
+    <RouterView />
   </div>
 </template>
 
