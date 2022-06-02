c186b5c2dcdbb4d897f647d7ab617e5f13ce1989
Ronald <rjskinnerc@unal.edu.co>
2022-06-01 20:49:48 -0500

    Add Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav className="md:flex justify-between items-center md:mx-5 lg:w-4/5 lg:mx-auto">
+    <h1 className="text-rose-500 text-3xl text-center py-10 font-bold" :style="{
+      textShadow: '1px 1px 1px rgba(206,206,206,0.7)'
+    }">Math Magicians</h1>
+    <ul className="flex justify-around md:gap-x-5">
+      <li>
+        <RouterLink to="/" class="text-rose-500 hover:text-rose-400 font-semibold p-2">Home</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/calculator" class="text-rose-500 hover:text-rose-400 font-semibold p-2">
+          Calculator</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/quote" class="text-rose-500 hover:text-rose-400 font-semibold p-2">
+          Quote</RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
