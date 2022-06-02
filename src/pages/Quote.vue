626c2f97dd7b118ab786066b38485fbd259efc5f
Ronald <rjskinnerc@unal.edu.co>
2022-06-01 21:10:30 -0500

    Add quote page and quotes array

---
+++ src/pages/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <div className="p-5 lg:w-4/5 lg:mx-auto lg:p-0 lg:mt-24">
+
+    <p className="text-sky-100 font-semibold italic text-2xl mt-24 text-right">
+      &ldquo;
+      {{ quote.quote }}
+      &rdquo;
+    </p>
+    <p className="text-zinc-400 text-lg text-right italic my-5">
+      —
+      {{ quote.author }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { getAnyQuote } from '../logic/quotes';
+import { onMounted, onUnmounted, ref } from 'vue';
+
+const quote = ref(getAnyQuote());
+let rotator;
+
+onMounted(() => {
+  rotator = setInterval(() => {
+    quote.value = getAnyQuote();
+  }, 5000)
+})
+
+onUnmounted(() => clearInterval(rotator))
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from '../pages/Home.vue'
 import Calculator from '../pages/Calculator.vue'
+import Quote from '../pages/Quote.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/calculator',
       name: 'calculator',
       component: Calculator,
+    },
+    {
+      path: '/quote',
+      name: 'quote',
+      component: Quote,
     }
   ]
 })
